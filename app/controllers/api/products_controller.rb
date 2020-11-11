97e61a356376133199c0319d470c4f815cf4694a
jimmy <jonssonjimmy8@gmail.com>
2020-11-10 19:11:51 -0500

    finishing index action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,7 +1,8 @@
 class Api::ProductsController < ApplicationController
   def index
-    @message = "product index"
-    # @products = Product.all
+    @products = Product.all
     render "index.json.jb"
   end
+
+
 end
