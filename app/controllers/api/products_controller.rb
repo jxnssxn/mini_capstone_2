8a0b0ad4f37bc47d097f2bd5b01637c662ce0d3d
jimmy <jonssonjimmy8@gmail.com>
2020-11-11 18:07:18 -0500

    Initial commit created db, index and create actions with routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,8 +1,19 @@
 class Api::ProductsController < ApplicationController
+
   def index
     @products = Product.all
     render "index.json.jb"
   end
 
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    )
+    @product.save
+    render "create.json.jb"
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   namespace :api do
     get "/products" => "products#index"
 
-    
+    post "/products" => "products#create"
   end
 end
